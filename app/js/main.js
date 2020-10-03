4162bd41ab837c2bef482f1ee0b15739a425ea59
Nikita <verstka.avacode@inbox.ru>
2020-10-03 11:19:38 +0300

    first_program

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,27 @@
-$( () => {
-    
+$(() => {
+	const numberOfFilms = prompt("Сколько Фильмов вы уже посмотрели?", "");
 
 
-  
+	const personalMovieDB = {
+		count: numberOfFilms,
+		movies: {},
+		actors: {},
+		genres: [],
+		privat: false
+	};
+
+	const a = prompt("Один из последних фильмов, которые вы посмотрели?", ""),
+		b = prompt("На сколько можете его оценить?", ""),
+		c = prompt("Один из последних фильмов, которые вы посмотрели?", ""),
+		d = prompt("На сколько можете его оценить?", "");
+
+	personalMovieDB.movies[a] = b;
+	personalMovieDB.movies[c] = d;
+
+	console.log(personalMovieDB);
+
+
+
+
+
 });
