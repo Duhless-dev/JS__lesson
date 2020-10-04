8d0ec08c8018869a1d6646c83e4fbc26c7c024ba
Nikita <verstka.avacode@inbox.ru>
2020-10-04 04:40:03 +0300

    second_program

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
+//const {
+//	stream
+//} = require("browser-sync");
+
 $(() => {
-	const numberOfFilms = prompt("Сколько Фильмов вы уже посмотрели?", "");
+	const numberOfFilms = +prompt("Сколько Фильмов вы уже посмотрели?", "");
+
+	if (numberOfFilms < 10) {
+		alert('Просмотрено довольно мало Фильмов');
+	} else if (numberOfFilms > 10 && numberOfFilms < 30) {
+		alert('Обычный зритель');
+	} else if (numberOfFilms >= 30) {
+		alert('Киноман');
+	} else {
+		alert('Произошла ошибка');
+	}
 
 
 	const personalMovieDB = {
@@ -10,14 +24,23 @@ $(() => {
 		privat: false
 	};
 
-	const a = prompt("Один из последних фильмов, которые вы посмотрели?", ""),
-		b = prompt("На сколько можете его оценить?", ""),
-		c = prompt("Один из последних фильмов, которые вы посмотрели?", ""),
-		d = prompt("На сколько можете его оценить?", "");
+	for (let i = 0; i < 2; i++) {
+		let a = prompt("Один из последних фильмов, которые вы посмотрели?", "");
+		if (a == null || a.length == 0 || a.length > 50) {
+			alert('Данные не подходят под стандарт');
+			i--;
+		} else {
+			let b = prompt("Насолько вы можете его оценить?", "");
+			if (b == null || b.length == 0 || b.length > 50) {
+				alert('Данные не подходят под стандарт');
+				i--;
+			} else {
+				personalMovieDB.movies[a] = b;
+			}
+		}
 
-	personalMovieDB.movies[a] = b;
-	personalMovieDB.movies[c] = d;
 
+	}
 	console.log(personalMovieDB);
 
 
